8be0a23c5fe34fe10b48c4f7fc369147042ca63d
tamaumi <hai.huynh@smartdev.com>
2022-04-11 10:49:00 +0700

    first commit

---
+++ src/main/java/com/kis/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.kis.repository;
+
+import com.kis.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
